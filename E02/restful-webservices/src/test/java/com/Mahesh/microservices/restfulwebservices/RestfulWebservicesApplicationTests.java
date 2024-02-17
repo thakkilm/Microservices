a906c72aa40d5bf3d8dfd031a3878fcd8549f199
Mahesh Thakkilapati <thakkilm@mail.gvsu.edu>
2024-02-17 13:45:49 -0600

    Initialize E02 and ignore E01

---
+++ E02/restful-webservices/src/test/java/com/Mahesh/microservices/restfulwebservices/RestfulWebservicesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.Mahesh.microservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}

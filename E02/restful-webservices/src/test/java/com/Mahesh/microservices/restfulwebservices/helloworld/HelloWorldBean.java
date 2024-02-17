1aecb9689d8265bcfb2671fcf9b1d1a9b2f71831
Mahesh Thakkilapati <thakkilm@mail.gvsu.edu>
2024-02-17 13:47:29 -0600

    Create User classes and set error response code for not found resources

---
+++ E02/restful-webservices/src/test/java/com/Mahesh/microservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package com.Mahesh.microservices.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+    private String message;
+
+
+    public HelloWorldBean(String helloWorld) {
+        this.message=helloWorld;
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
---
+++ E02/restful-webservices/src/test/java/com/Mahesh/microservices/restfulwebservices/user/User.java
@@ -0,0 +1,49 @@
+package com.Mahesh.microservices.restfulwebservices.user;
+
+import java.time.LocalDate;
+
+public class User {
+ private Integer id;
+ private String name;
+ private LocalDate date;
+
+    public User(Integer id, String name, LocalDate date) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}

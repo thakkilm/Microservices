305a78b5245b8b526742c3a918f4148f949be6be
MAHESH THAKKILAPATI <maheshthakkilapati@MAHESHs-MBP.lan>
2024-02-27 16:18:46 -0600

    Latest Changes

--- E02/restful-webservices/src/main/java/com/Mahesh/microservices/restfulwebservices/helloworld/HelloWorldController.java
+++ E02/restful-webservices/src/main/java/com/Mahesh/microservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,20 +1,39 @@
 package com.Mahesh.microservices.restfulwebservices.helloworld;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("hello-world")
     public String helloWorld(){
-        return "Hello World";
+        return "Hello World Mahesh";
     }
 
     @GetMapping("hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("hello-world-internationalized")
+    public String helloWorldInternationalized(){
+
+        Locale locale= LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default Message",locale);
+
+//        return new HelloWorldBean("Good Morning");
     }
 
 }

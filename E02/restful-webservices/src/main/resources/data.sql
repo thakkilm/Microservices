95972a79964ca11272f34814e478c93f4ef94f3f
MAHESH THAKKILAPATI <maheshthakkilapati@MAHESHs-MBP.lan>
2024-03-13 10:17:20 -0500

    Connect H2 Data Base

---
+++ E02/restful-webservices/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details   (id,date,name)
+values (1001,current_date(),'Mahesh');
+
+insert into user_details   (id,date,name)
+values (1002,current_date(),'Lokesh');
+
+insert into user_details   (id,date,name)
+values (1003,current_date(),'Chaitu');

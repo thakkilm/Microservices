fda84d953559a2a8515bc9fc16bba5744b6cde1b
thakkilm <mahesh.t1896@gmail.com>
2024-03-27 22:29:54 -0400

    Add comment

--- E02/restful-webservices/src/main/java/com/Mahesh/microservices/restfulwebservices/RestfulWebservicesApplication.java
+++ E02/restful-webservices/src/main/java/com/Mahesh/microservices/restfulwebservices/RestfulWebservicesApplication.java
@@ -2,7 +2,7 @@ package com.Mahesh.microservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// New comment
 @SpringBootApplication
 public class RestfulWebservicesApplication {
 
